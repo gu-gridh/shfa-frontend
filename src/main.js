29cc9ec51c1d3f0e28f5b54d2600db10d615f617
Tristan Bridge <tbridge360@gmail.com>
2025-04-17 09:21:26 +0200

    clean up unneeded packages

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router.js'
-import MasonryWall from '@yeger/vue-masonry-wall'
 import matomo from 'vue-matomo';
 import messages from './i18n/messages'
 import { createI18n } from 'vue-i18n';
@@ -18,10 +17,8 @@ const i18n = createI18n({
 })
 
 app.use(router)
-app.use(MasonryWall)
 app.use(i18n);
 app.use(pinia);
-
 
 // Use the Matomo plugin only if configured in env.
 if (import.meta.env.VITE_MATOMO_URL && import.meta.env.VITE_MATOMO_ID) {
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,18 +7,4 @@ module.exports = {
   theme: {
     extend: {},
   },
-  // add daisyUI plugin
-  plugins: [require("daisyui")],
-
-  // daisyUI config (optional)
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dark",
-  },
 }
