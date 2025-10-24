49bc99a0e880436c0860da4e47e69620cb449938
Tristan Bridge <tbridge360@gmail.com>
2025-10-24 09:06:21 +0200

    viewer cleans up

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -22,7 +22,8 @@
       </div>
     </div>
     <a id="Download">
-      <div id="DownloadButton" class="NavButton round-button download-button compact" title="Download image" @click="downloadImage"></div>
+      <div id="DownloadButton" class="NavButton round-button download-button compact" title="Download image"
+        @click="downloadImage"></div>
     </a>
   </div>
   <!-- Sidebuttons -->
@@ -37,184 +38,191 @@
 </template>
 
 <script>
-  import OpenSeadragon from "../external/bookmark-url.js";
+import OpenSeadragon from "../external/bookmark-url.js";
 
-  export default {
-    props: {
-      iiifFile: {
-        type: String,
-        required: true,
-        default: '',
-      },
+export default {
+  props: {
+    iiifFile: {
+      type: String,
+      required: true,
+      default: '',
     },
-    data() {
-      return {
-        completeUrl: null,
-        viewer: null,
-        link_3d: false,
-      };
+  },
+  data() {
+    return {
+      completeUrl: null,
+      viewer: null,
+      link_3d: false,
+    };
+  },
+  mounted() {
+    this.fetchImageData();
+    this.toggleFullScreenPageMode(false);
+  },
+  methods: {
+    toggleFullScreenPageMode(bool) {
+      //toggles full screen and full page modes
+      OpenSeadragon.supportsFullScreen = bool;
+      OpenSeadragon.supportsFullPage = !bool;
     },
-    mounted() {
-      this.fetchImageData();
-      this.toggleFullScreenPageMode(false);
-    },
-    methods: {
-      toggleFullScreenPageMode(bool) {
-        //toggles full screen and full page modes
-        OpenSeadragon.supportsFullScreen = bool;
-        OpenSeadragon.supportsFullPage = !bool;
-      },
-      async fetchImageData() {
-        if (!this.iiifFile) return;
+    async fetchImageData() {
+      if (!this.iiifFile) return;
 
-        try {
-          const resp = await fetch(
-            `https://dev-shfa.dh.gu.se/api/image/?id=${encodeURIComponent(this.iiifFile)}&depth=1`
-          );
-          if (!resp.ok) throw new Error(`HTTP ${resp.status}`);
+      try {
+        const resp = await fetch(
+          `https://dev-shfa.dh.gu.se/api/image/?id=${encodeURIComponent(this.iiifFile)}&depth=1`
+        );
+        if (!resp.ok) throw new Error(`HTTP ${resp.status}`);
 
-          const first = (await resp.json()).results?.[0];
-          if (!first) {
-            console.warn('No image returned for', this.iiifFile);
-            return;
-          }
-          const site = first.site ?? {};
-          this.completeUrl = first.file;
-          this.lamning_id = site.lamning_id ?? '';
-          this.raa_id = site.raa_id ?? '';
-          this.placename = site.placename ?? '';
-          // this.creator = first.author?.name || 'Unknown';
-          this.img_id = first.id;
-          this.link_3d = !!first.group;
-          this.query_3d = first.group?.text ?? null;
-          const infoUrl = `${first.iiif_file}/info.json`;
-          this.viewer ? this.viewer.open(infoUrl) : this.initOpenSeadragon(first.iiif_file);
-          this.people = first.people?.map(people => people?.name.replace(',','').replace(', ','')).join('')
-        } catch (err) {
-          console.error('Fetch error:', err);
+        const first = (await resp.json()).results?.[0];
+        if (!first) {
+          console.warn('No image returned for', this.iiifFile);
+          return;
         }
-      },
-      open3dViewer() {
-        const threedUrl = `https://dev-shfa.dh.gu.se/viewer/?q=${this.query_3d}/mesh`;
-        window.open(threedUrl, "_blank");
-      },
-      downloadImage() {
-        const imageUrl = this.completeUrl;
-        const lamning_id = this.lamning_id;
-        const raa_id = this.raa_id;
-        const placename = this.placename;
-        const authors = this.people;
-        const img_id = this.img_id;
-        // const year = this.year;
-
-        if (window._paq) _paq.push(['trackLink', imageUrl, 'download']);  //track download with matomo if available
-
-        fetch(imageUrl)
-          .then((response) => response.blob())
-          .then((blob) => {
-            const url = window.URL.createObjectURL(blob);
-            // const imgId = imageUrl.split("/").pop();
-            const downloadName = `${authors}_${lamning_id || raa_id || placename
-              }_SHFAid${img_id}`;
-            const a = document.createElement("a");
-            a.style.display = "none";
-            a.href = url;
-            a.download = downloadName || "carving.jpg"; //use the extracted file name or a default
-            document.body.appendChild(a);
-            a.click();
-            window.URL.revokeObjectURL(url);
-            document.body.removeChild(a);
-          })
-          .catch(() => console.error("Could not download the image"));
-      },
-
-      initOpenSeadragon(iiifFile) {
-        this.viewer = OpenSeadragon({
-          element: this.$refs.imageViewer,
-          prefixUrl: "/path/to/openseadragon/images/",
-          tileSources: `${iiifFile}/info.json`,
-          immediateRender: false,
-          visibilityRatio: 0.6,
-          minZoomImageRatio: 0.5,
-          homeFillsViewer: false,
-          showZoomControl: true,
-          showHomeControl: true,
-          //defaultZoomLevel:0.6,
-          showFullPageControl: true,
-          showNavigator: true,
-          navigatorAutoFade: true,
-          //navigatorId:   "navigator-div",
-          fullPageButton: "full-page",
-          zoomInButton: "zoom-in",
-          zoomOutButton: "zoom-out",
-          homeButton: "home",
-        });
-
-        this.viewer.addHandler("full-page");
-
-        /* this.viewer.bookmarkUrl({
-          trackPage: true,
-        }); */
-      },
+        const site = first.site ?? {};
+        this.completeUrl = first.file;
+        this.lamning_id = site.lamning_id ?? '';
+        this.raa_id = site.raa_id ?? '';
+        this.placename = site.placename ?? '';
+        // this.creator = first.author?.name || 'Unknown';
+        this.img_id = first.id;
+        this.link_3d = !!first.group;
+        this.query_3d = first.group?.text ?? null;
+        const infoUrl = `${first.iiif_file}/info.json`;
+        this.viewer ? this.viewer.open(infoUrl) : this.initOpenSeadragon(first.iiif_file);
+        this.people = first.people?.map(people => people?.name.replace(',', '').replace(', ', '')).join('')
+      } catch (err) {
+        console.error('Fetch error:', err);
+      }
     },
-    watch: {
-      iiifFile(newIiif, oldIiif) {
-        if (newIiif !== oldIiif) {
-          this.fetchImageData();
-        }
-      },
+    open3dViewer() {
+      const threedUrl = `https://dev-shfa.dh.gu.se/viewer/?q=${this.query_3d}/mesh`;
+      window.open(threedUrl, "_blank");
     },
-  };
+    downloadImage() {
+      const imageUrl = this.completeUrl;
+      const lamning_id = this.lamning_id;
+      const raa_id = this.raa_id;
+      const placename = this.placename;
+      const authors = this.people;
+      const img_id = this.img_id;
+      // const year = this.year;
+
+      if (window._paq) _paq.push(['trackLink', imageUrl, 'download']);  //track download with matomo if available
+
+      fetch(imageUrl)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const url = window.URL.createObjectURL(blob);
+          // const imgId = imageUrl.split("/").pop();
+          const downloadName = `${authors}_${lamning_id || raa_id || placename
+            }_SHFAid${img_id}`;
+          const a = document.createElement("a");
+          a.style.display = "none";
+          a.href = url;
+          a.download = downloadName || "carving.jpg"; //use the extracted file name or a default
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+          document.body.removeChild(a);
+        })
+        .catch(() => console.error("Could not download the image"));
+    },
+
+    initOpenSeadragon(iiifFile) {
+      this.viewer = OpenSeadragon({
+        element: this.$refs.imageViewer,
+        prefixUrl: "/path/to/openseadragon/images/",
+        tileSources: `${iiifFile}/info.json`,
+        immediateRender: false,
+        visibilityRatio: 0.6,
+        minZoomImageRatio: 0.5,
+        homeFillsViewer: false,
+        showZoomControl: true,
+        showHomeControl: true,
+        //defaultZoomLevel:0.6,
+        showFullPageControl: true,
+        showNavigator: true,
+        navigatorAutoFade: true,
+        //navigatorId:   "navigator-div",
+        fullPageButton: "full-page",
+        zoomInButton: "zoom-in",
+        zoomOutButton: "zoom-out",
+        homeButton: "home",
+      });
+
+      this.viewer.addHandler("full-page");
+
+      /* this.viewer.bookmarkUrl({
+        trackPage: true,
+      }); */
+    },
+  },
+  watch: {
+    iiifFile(newIiif, oldIiif) {
+      if (newIiif !== oldIiif) {
+        this.fetchImageData();
+      }
+    },
+  },
+  beforeUnmount() {
+    if (this.viewer) {
+      this.viewer.destroy();
+      this.viewer = null;
+    }
+  },
+
+};
 </script>
 
 <style scoped>
-  a:visited {
-    outline: none;
-  }
+a:visited {
+  outline: none;
+}
 
-  a:focus {
-    outline: none;
-  }
+a:focus {
+  outline: none;
+}
 
-  a:active {
-    outline: none;
-  }
+a:active {
+  outline: none;
+}
 
+.image-viewer {
+  width: auto;
+  margin-left: 25px;
+  margin-right: 30px;
+  margin-top: -45px;
+  height: 65%;
+  background-color: var(--page-background-accent);
+  border-radius: 2px;
+  overflow: hidden;
+  z-index: 1;
+}
+
+@media (max-width: 600px) {
   .image-viewer {
-    width: auto;
-    margin-left: 25px;
-    margin-right: 30px;
-    margin-top: -45px;
-    height: 65%;
-    background-color: var(--page-background-accent);
-    border-radius: 2px;
-    overflow: hidden;
-    z-index: 1;
+    margin-left: 0px;
+    margin-right: 0px;
+    padding: 0px;
+    width: 100%;
   }
+}
 
-  @media (max-width: 600px) {
-    .image-viewer {
-      margin-left: 0px;
-      margin-right: 0px;
-      padding: 0px;
-      width: 100%;
-    }
-  }
+.image-viewer.fullpage {
+  height: 100vh !important;
+  width: 100vw !important;
+}
 
-  .image-viewer.fullpage {
-    height: 100vh !important;
-    width: 100vw !important;
-  }
+#navigatorDiv {
+  position: absolute;
+  width: 80px;
+  height: 80px;
+  margin-top: 10px;
+  margin-right: 10px;
+}
 
-  #navigatorDiv {
-    position: absolute;
-    width: 80px;
-    height: 80px;
-    margin-top: 10px;
-    margin-right: 10px;
-  }
-
-  /*
+/*
 #ToolbarHorizontal {
   position: absolute;
   display: flex;
@@ -305,7 +313,7 @@
 }
 */
 
-  #threedViewerButton {
+#threedViewerButton {
   position: relative;
   top: 30px;
   background-size: 100%;
@@ -320,9 +328,9 @@
   overflow: hidden;
   cursor: pointer;
   color: var(--popup-text);
-} 
+}
 
-  /* .NavButton {
+/* .NavButton {
   border-radius: 50%;
   width: 35px;
   height: 35px;
@@ -339,25 +347,25 @@
 } */
 
 .button-icon:hover {
-    opacity: 1.0;
+  opacity: 1.0;
 }
 
-.round-button{
-    width: 45px;
-    height: 45px;
-    cursor: pointer;
-    position: absolute;
-    left: 10px;
-    background-color: rgb(0, 0, 0, 1.0) !important;
-    border-radius: 50%;
-    user-select: none;
-    -webkit-user-select: none;
+.round-button {
+  width: 45px;
+  height: 45px;
+  cursor: pointer;
+  position: absolute;
+  left: 10px;
+  background-color: rgb(0, 0, 0, 1.0) !important;
+  border-radius: 50%;
+  user-select: none;
+  -webkit-user-select: none;
 }
 
 .round-button:hover {
-    background-color: rgb(0, 0, 0, 1.0) !important;
-    cursor: pointer;
-    transform: scale(1.07);
+  background-color: rgb(0, 0, 0, 1.0) !important;
+  cursor: pointer;
+  transform: scale(1.07);
 }
 
 /* 
@@ -369,51 +377,51 @@
   }
  */
 
-  .interface-area{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: end;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    bottom: 15px;
-    pointer-events: none;
-    z-index: 10000;
-  }
+.interface-area {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: end;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  bottom: 15px;
+  pointer-events: none;
+  z-index: 10000;
+}
 
-  .toolbar-bottom-mini {
-    pointer-events: auto;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    padding-left: 6px;
-    padding-right: 6px;
-    width: auto;
-    height: 42px;
-    background-color: rgb(0, 0, 0, 0.8);
-    border-radius: 8px;
-    z-index: 1000;
-    cursor: pointer;
-    -webkit-user-select: none;
-    -webkit-touch-callout: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    touch-action: none;
-    -ms-touch-action: none;
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    -webkit-tap-highlight-color: transparent;
-    box-shadow: 0.0rem 0.1rem 1rem rgba(0, 0, 0, 0.3) !important;
-    border-style: solid;
-    border-color: grey;
-    border-width: 0.5px 0px 0px 0px;
-    -webkit-backdrop-filter: blur(3px);
-    backdrop-filter: blur(3px);
-  }
+.toolbar-bottom-mini {
+  pointer-events: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding-left: 6px;
+  padding-right: 6px;
+  width: auto;
+  height: 42px;
+  background-color: rgb(0, 0, 0, 0.8);
+  border-radius: 8px;
+  z-index: 1000;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -webkit-touch-callout: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  touch-action: none;
+  -ms-touch-action: none;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  -webkit-tap-highlight-color: transparent;
+  box-shadow: 0.0rem 0.1rem 1rem rgba(0, 0, 0, 0.3) !important;
+  border-style: solid;
+  border-color: grey;
+  border-width: 0.5px 0px 0px 0px;
+  -webkit-backdrop-filter: blur(3px);
+  backdrop-filter: blur(3px);
+}
 
-  #DownloadButton {
+#DownloadButton {
   position: absolute;
   bottom: 12px;
   background: url(https://data.dh.gu.se/ui-icons/download_white.png);
@@ -423,128 +431,128 @@
   background-color: var(--viewer-button-background);
   overflow: hidden;
   cursor: pointer;
-  z-index:1000;
-} 
+  z-index: 1000;
+}
 
 
-  #Home {
-    background: url(https://data.dh.gu.se/ui-icons/frame_small_white.png);
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    width: 28px;
-    height: 28px;
-    cursor: pointer;
-    overflow: hidden;
-  }
+#Home {
+  background: url(https://data.dh.gu.se/ui-icons/frame_small_white.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 28px;
+  height: 28px;
+  cursor: pointer;
+  overflow: hidden;
+}
 
-  #ZoomIn {
-    background: url(https://data.dh.gu.se/ui-icons/zoomin_big_white.png);
-    background-size: 100%;
-    background-repeat: no-repeat;
-    background-position: center;
-    width: 28px;
-    height: 28px;
-    cursor: pointer;
-    overflow: hidden;
-  }
+#ZoomIn {
+  background: url(https://data.dh.gu.se/ui-icons/zoomin_big_white.png);
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 28px;
+  height: 28px;
+  cursor: pointer;
+  overflow: hidden;
+}
 
-  #ZoomOut {
-    background: url(https://data.dh.gu.se/ui-icons/zoomout_big_white.png);
-    background-size: 100%;
-    background-repeat: no-repeat;
-    background-position: center;
-    width: 28px;
-    height: 28px;
-    cursor: pointer;
-    overflow: hidden;
-  }
+#ZoomOut {
+  background: url(https://data.dh.gu.se/ui-icons/zoomout_big_white.png);
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 28px;
+  height: 28px;
+  cursor: pointer;
+  overflow: hidden;
+}
 
-  #FullPage {
-    background: url(https://data.dh.gu.se/ui-icons/expand_white.svg);
-    background-size: 100%;
-    background-repeat: no-repeat;
-    background-position: center;
-    width: 26px;
-    height: 26px;
-    overflow: hidden;
-    cursor: pointer;
-  }
+#FullPage {
+  background: url(https://data.dh.gu.se/ui-icons/expand_white.svg);
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 26px;
+  height: 26px;
+  overflow: hidden;
+  cursor: pointer;
+}
 
-  .NavButton {
-    pointer-events: auto;
-    width: 35px;
-    height: 35px;
-    color: white;
-    opacity: 0.8;
-    margin-right: 6px;
-    margin-left: 6px;
-    margin-top: 6px;
-    margin-bottom: 6px;
-    cursor: pointer;
-    user-select: none;
-    -webkit-user-select: none;
-    opacity: 0.8;
-  }
+.NavButton {
+  pointer-events: auto;
+  width: 35px;
+  height: 35px;
+  color: white;
+  opacity: 0.8;
+  margin-right: 6px;
+  margin-left: 6px;
+  margin-top: 6px;
+  margin-bottom: 6px;
+  cursor: pointer;
+  user-select: none;
+  -webkit-user-select: none;
+  opacity: 0.8;
+}
 
-  .NavButton:hover {
-    opacity: 1;
-    cursor: pointer;
-    background-color:transparent;
-  }
+.NavButton:hover {
+  opacity: 1;
+  cursor: pointer;
+  background-color: transparent;
+}
 
-  #Prev,
-  #Next {
-    position: relative;
-    width: 25px;
-    height: 15px;
-    background-size: 35px !important;
-    background-repeat: no-repeat;
-    background-position: center;
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+#Prev,
+#Next {
+  position: relative;
+  width: 25px;
+  height: 15px;
+  background-size: 35px !important;
+  background-repeat: no-repeat;
+  background-position: center;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  #Prev {
-    background-image: url(https://data.dh.gu.se/ui-icons/arrow_prev_white.png);
-  }
+#Prev {
+  background-image: url(https://data.dh.gu.se/ui-icons/arrow_prev_white.png);
+}
 
-  #Next {
-    background-image: url(https://data.dh.gu.se/ui-icons/arrow_next_white.png);
-    margin-left: 10px;
-  }
+#Next {
+  background-image: url(https://data.dh.gu.se/ui-icons/arrow_next_white.png);
+  margin-left: 10px;
+}
 
-  #currentpage {
-    font-family: "Oswald", sans-serif !important;
-    color: white;
-    font-size: 0.8em !important;
-    font-weight: 100;
-    pointer-events: none !important;
-  }
+#currentpage {
+  font-family: "Oswald", sans-serif !important;
+  color: white;
+  font-size: 0.8em !important;
+  font-weight: 100;
+  pointer-events: none !important;
+}
 
-  .switch-button {
-    pointer-events: auto;
-    width: 35px;
-    height: 35px;
-    color: white;
-    opacity: 0.8;
-    margin-right: 8px;
-    margin-left: 8px;
-    margin-top: 8px;
-    margin-bottom: 8px;
-    cursor: pointer;
-    user-select: none;
-    -webkit-user-select: none;
-  }
+.switch-button {
+  pointer-events: auto;
+  width: 35px;
+  height: 35px;
+  color: white;
+  opacity: 0.8;
+  margin-right: 8px;
+  margin-left: 8px;
+  margin-top: 8px;
+  margin-bottom: 8px;
+  cursor: pointer;
+  user-select: none;
+  -webkit-user-select: none;
+}
 
-  .switch-button:hover {
-    opacity: 1;
-    cursor: pointer;
-  }
+.switch-button:hover {
+  opacity: 1;
+  cursor: pointer;
+}
 
-  *:focus {
-    outline: none !important;
-  }
+*:focus {
+  outline: none !important;
+}
 </style>
